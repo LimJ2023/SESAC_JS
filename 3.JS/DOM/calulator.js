86d0eca149bdda851249a5d090f1bfb94675cd1f
Lim J <heuristic2022@gmail.com>
2024-10-16 20:14:27 +0900

    초기화 0문제 해결

--- 3.JS/DOM/calulator.js
+++ 3.JS/DOM/calulator.js
@@ -11,11 +11,14 @@ document.addEventListener("DOMContentLoaded", function () {
         case "*":
         case "/":
           console.log("연산자 진입 : " + result.slice(-1));
-          if (result == "") {
+          if (result == " ") {
             console.log("아무것도 없을 때 계산할 수 없습니다.");
             inputNum.value = result;
+            break;
           }
-          //result에 공백 넣은 이유 : 이거 없으면 includes(result.slice(-1))<-얘가 터짐
+          //result에 공백 넣은 이유 :
+          //이거 없으면 includes(result.slice(-1))<-얘가 터짐 (아마 슬라이스할 요소가 없으니까 터지는걸로 예상)
+          //이거 해결 못해서 result 초기값에 공백 넣음 하
           if (result.length == 1 || "+-*/".includes(result.slice(-1))) {
             console.log("인클루드 진입 : ", result);
             result = result.slice(0, -1) + input;
@@ -26,18 +29,19 @@ document.addEventListener("DOMContentLoaded", function () {
           }
           break;
         case "=":
+          //여기에 공백 넣어줘야 초기세팅과 같아짐...
           result = eval(result) + " ";
           console.log("결과 : " + result);
-          //뒤에 공백 넣어줘야 = 했을 때 문제 안생김 ㅋㅋ
+          //infinity 뒤에 공백 넣어줘야 = 했을 때 문제 안생김 ㅋㅋ
           if (result == "Infinity ") {
             console.log("0으로 나눌 수 없습니다.");
-            result = "0";
+            result = " ";
           }
           inputNum.value = result;
           break;
         case "C":
-          result = "";
-          inputNum.value = result;
+          result = " ";
+          inputNum.value = " ";
           break;
 
         default:
