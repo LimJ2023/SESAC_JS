62fd6f2407a74f648b2678763f8975da28df5fef
Lim J <heuristic2022@gmail.com>
2024-10-18 15:37:41 +0900

    투두리스트 모두삭제추가

--- 3.JS/ToDoList/todo.js
+++ 3.JS/ToDoList/todo.js
@@ -1,6 +1,8 @@
 const input = document.getElementById("todoInput");
 const list = document.getElementById("todoList");
 const addBtn = document.getElementById("addButton");
+const delAllBtn = document.getElementById("delAllButton");
+
 addBtn.addEventListener("click", () => {
   const li = document.createElement("li");
   const del = document.createElement("button");
@@ -10,7 +12,6 @@ addBtn.addEventListener("click", () => {
   check.addEventListener("change", (e) => {
     //ㅋㅋㅋㅋchecked 속성이 당연히 있을줄 알앗지
     // mdn에 진짜 있음. https://developer.mozilla.org/ko/docs/Web/HTML/Element/input/checkbox
-    console.log(e.target.checked);
     if (e.target.checked) {
       li.style.textDecoration = "line-through";
     } else {
@@ -23,9 +24,27 @@ addBtn.addEventListener("click", () => {
   });
 
   //만들 때 텍스트 안에 i를 더 만들어버렸으니 <i><i></i></i>이런식으로 되어있었지...이걸몰랏네
-  li.append(check);
-  li.append(input.value);
-  li.appendChild(del);
+  // li.appendChild(check);
+  // li.appendChild(input.value);
+  // li.appendChild(del);
+  //append를 이용해서 한번에 넣는게 가능한가?
+  li.append(check, input.value, del);
   list.appendChild(li);
   input.value = "";
+});
+
+delAllBtn.addEventListener("click", () => {
+  const lis = document.getElementsByTagName("li");
+  console.log(lis);
+  //왜 하나가 남냐...이유가 뭐지
+  // for (item of lis) {
+  //   item.remove();
+  // }
+  //HTMLCollection을 반환하는데 실시간 동기화되는 컬렉션이기 때문이다.
+  // 요소를 삭제할때마다 길이가 동적으로 줄어드는데, for문의 인덱스는 넘어갔기 때문에
+  // 두번째 요소만 삭제하게 되는 문제가 발생하는 것...
+  // 해결하기 위해 역순 for문을 작성한다.
+  for (let i = lis.length - 1; i >= 0; i--) {
+    lis[i].remove();
+  }
 });
--- 3.JS/chrome_extension/calculator.js
+++ 3.JS/chrome_extension/calculator.js
@@ -13,8 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
         case "/":
           if (result == " ") {
             text.textContent = "연산할 수 있는 수가 없습니다.";
-            console.log("연산할 수 있는 수가 없습니다.");
-            //보이는건 0으로..
             inputNum.value = "0";
             break;
           }
@@ -32,13 +30,11 @@ document.addEventListener("DOMContentLoaded", function () {
         case "=":
           //여기에 공백 넣어줘야 초기세팅과 같아짐...
           result = eval(result) + " ";
-          console.log("결과 저장중 " + result.slice(1, -1));
           //infinity 뒤에 공백 넣어줘야 = 했을 때 문제 안생김 ㅋㅋ
           // if (result == "Infinity ") {
           //더 좋은 방법이 떠오름. 결과값에 무한대가 있으면 무조건 문제가 있는걸로 변경
           if ("Infinity ".includes(result)) {
-            text.textContent = "0으로 나눌 수 없습니다.";
-            console.log("0으로 나눌 수 없습니다.");
+            text.textContent = "0으로는 나눌 수 없습니다.";
             result = " ";
             inputNum.value = "0";
           } else {
@@ -60,7 +56,6 @@ document.addEventListener("DOMContentLoaded", function () {
             result = result + input;
             inputNum.value = result;
           }
-          console.log("결과 저장중 " + result);
           break;
       }
     });
