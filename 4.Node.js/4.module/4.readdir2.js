5c87fe38713bba6741c20d404f0ea0839cec2b7b
Lim J <149343911+LimJ2023@users.noreply.github.com>
2024-10-23 22:52:50 +0900

    폴더구조 과제

--- 4.Node.js/4.module/4.readdir2.js
+++ 4.Node.js/4.module/4.readdir2.js
@@ -3,12 +3,13 @@ const path = require("path");
 const directoryPath = "../3.class";
 
 fs.readdir(directoryPath, "utf-8", (err, files) => {
-  if (err) return console.log("읽기 실패", err);
-  files.forEach((file) => {
-    const filePath = path.join(directoryPath, file);
-    fs.readFile(filePath, "utf-8", (err, data) => {
-      if (err) return console.log(err);
-      console.log(data);
+    if (err) return console.log("읽기 실패", err);
+    files.forEach((file) => {
+        const filePath = path.join(directoryPath, file);
+        fs.readFile(filePath, "utf-8", (err, data) => {
+            if (err) return console.log(err);
+            console.log(data);
+        });
     });
-  });
+    console.log(files);
 });
