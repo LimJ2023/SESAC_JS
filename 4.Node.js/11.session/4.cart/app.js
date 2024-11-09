a26c80dda568e5f57daab6f73c62fb40e838ac16
Lim J <heuristic2022@gmail.com>
2024-11-09 17:31:20 +0900

    11월 9일 과제 장바구니 만들기

--- 4.Node.js/11.session/4.cart/app.js
+++ 4.Node.js/11.session/4.cart/app.js
@@ -3,7 +3,7 @@ const session = require("express-session");
 const app = express();
 const path = require("path");
 
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
@@ -13,40 +13,104 @@ app.use(
     saveUninitialized: true, // 초기화되지 않은 세션을 저장소에 저장할것인가?
   })
 );
-
 //상품 db
 const products = [
   {
     id: 1,
     name: "바나나",
     price: "2000",
+    url: "/image/banana.jpg",
   },
   {
     id: 2,
     name: "사과",
     price: "3000",
+    url: "/image/apple.jpg",
   },
   {
     id: 3,
     name: "오렌지",
     price: "1500",
+    url: "/image/orange.jpg",
   },
 ];
 app.get("/products", (req, res) => {
   res.json(products);
 });
 
-app.get("/cart", (req, res) => {
+app.post("/cart", (req, res) => {
   const id = req.query.id;
-  const cart = req.session.cart || [];
+
+  // 주문한 물품이 상품목록에 있는 것인지 확인
   const product = products.find((e) => e.id == id);
+  req.session.cart = req.session.cart || [];
+  console.log("수량더한 카트 : ", req.session.cart);
   if (product) {
-    req.session.cart = [...cart, product];
-    console.log("세션에 저장된 목록 : ", req.session.cart);
-    res.json(req.session.cart);
+    // 카트에 같은 내용물이 있다면 quantity를 +1;
+    // 카트에서 quantity를 알아내는 방법?
+    if (
+      req.session.cart.length === 0 ||
+      !req.session.cart.find((e) => e.id == id)
+    ) {
+      product.quantity = 1;
+      req.session.cart.push(product);
+    } else {
+      req.session.cart.forEach((pro) => {
+        if (pro.id == id) {
+          pro.quantity = ++pro.quantity;
+        }
+      });
+    }
+    req.session.total = 0;
+    req.session.cart.forEach((pro) => {
+      req.session.total += Number(pro.price) * Number(pro.quantity);
+    });
+    //total을 카트 안에 넣어서 한번에 보내줄까? session에 total을 추가
+    console.log(
+      "세션에 저장된 목록 : ",
+      req.session.cart,
+      "total : ",
+      req.session.total
+    );
+    res.json({ products: req.session.cart, total: req.session.total });
   } else {
     res.status(404).json({ message: "잘못된 상품 번호 입니다." });
   }
+});
+
+// 수량 변경
+app.patch("/quantity", (req, res) => {
+  const reqId = req.body.id;
+  const reqCount = req.body.count;
+  req.session.cart.forEach((pro) => {
+    if (pro.id == reqId) {
+      pro.quantity = reqCount;
+      req.session.total = 0;
+      req.session.cart.forEach((pro) => {
+        req.session.total += Number(pro.price) * Number(pro.quantity);
+      });
+    }
+  });
+  console.log("수량변경후 ", req.session.cart);
+
+  //세션이 저장되지 않아서 생긴 문제였음(다른 fetch에선 수량이 제대로 업데이트 되지 않던 문제)
+  req.session.save();
+  res.json({ total: req.session.total });
+});
+
+app.delete("/delete", (req, res) => {
+  const id = req.query.id;
+  req.session.cart.splice(
+    0,
+    req.session.cart.length,
+    ...req.session.cart.filter((pro) => pro.id != id)
+  );
+  req.session.total = 0;
+  req.session.cart.forEach((pro) => {
+    req.session.total += Number(pro.price) * Number(pro.quantity);
+  });
+  req.session.save();
+  res.json({ products: req.session.cart, total: req.session.total });
 });
 app.listen(3000, () => {
   console.log("서버 레디");
