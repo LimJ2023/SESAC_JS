6447813a367e594416694ee29aab21b0bca53fcd
Lim J <149343911+LimJ2023@users.noreply.github.com>
2024-11-24 18:48:58 +0900

    로그인 구현

--- 6.CRM/crm_project_v2/routes/api/userRoute.js
+++ 6.CRM/crm_project_v2/routes/api/userRoute.js
@@ -8,105 +8,103 @@ const db = new sqlite3("db-sample.db");
 const LIMIT = Number(process.env.LIMIT);
 
 router.post("/users/", (req, res) => {
-  const { name, gender } = req.query;
-  const queryParams = [];
-  const countparams = [];
-  const curPage = Number(req.params.page) || 1;
-  const offset = (curPage - 1) * LIMIT;
+    const { name, gender } = req.query;
+    const queryParams = [];
+    const countparams = [];
+    const curPage = Number(req.params.page) || 1;
+    const offset = (curPage - 1) * LIMIT;
 
-  let query = `SELECT * FROM users WHERE 1=1`;
-  let countQuery = `SELECT count(*) as count FROM users WHERE 1=1`;
+    let query = `SELECT * FROM users WHERE 1=1`;
+    let countQuery = `SELECT count(*) as count FROM users WHERE 1=1`;
 
-  if (name) {
-    query += ` AND name = ?`;
-    countQuery += ` AND name = ?`;
-    queryParams.push(name);
-    countparams.push(name);
-  }
-  if (gender) {
-    query += ` AND gender = ?`;
-    countQuery += ` AND gender = ?`;
-    queryParams.push(gender);
-    countparams.push(gender);
-  }
+    if (name) {
+        query += ` AND name = ?`;
+        countQuery += ` AND name = ?`;
+        queryParams.push(name);
+        countparams.push(name);
+    }
+    if (gender) {
+        query += ` AND gender = ?`;
+        countQuery += ` AND gender = ?`;
+        queryParams.push(gender);
+        countparams.push(gender);
+    }
 
-  query += ` LIMIT ? OFFSET ?`;
-  queryParams.push(LIMIT);
-  queryParams.push(offset);
+    query += ` LIMIT ? OFFSET ?`;
+    queryParams.push(LIMIT);
+    queryParams.push(offset);
 
-  //아무 페이지값도 전달하지 않았을 땐 curPage가 1
-  const stmt = db.prepare(query);
-  const data = stmt.all(queryParams);
-  //어떻게하면 배열로 1,2,3,4,5,6 ... last - 3, last - 2, last - 1, ...이쁘게 전달해줄 수 있을까?
-  //전체페이지
-  const totalArr = paging(curPage, countQuery, countparams);
-  console.log(totalArr, "total ARR");
-  res.json({
-    data: data,
-    page: curPage,
-    totalArr: totalArr,
-    name: name,
-    gender: gender,
-  });
+    //아무 페이지값도 전달하지 않았을 땐 curPage가 1
+    const stmt = db.prepare(query);
+    const data = stmt.all(queryParams);
+    //어떻게하면 배열로 1,2,3,4,5,6 ... last - 3, last - 2, last - 1, ...이쁘게 전달해줄 수 있을까?
+    //전체페이지
+    const totalArr = paging(curPage, countQuery, countparams);
+    console.log(totalArr, "total ARR");
+    res.json({
+        data: data,
+        page: curPage,
+        totalArr: totalArr,
+        name: name,
+        gender: gender,
+    });
 });
 router.get("/users/:page", (req, res) => {
-  const { name, gender } = req.query;
-  const queryParams = [];
-  const countparams = [];
-  const curPage = Number(req.params.page) || 1;
-  const offset = (curPage - 1) * LIMIT;
+    const { name, gender } = req.query;
+    const queryParams = [];
+    const countparams = [];
+    const curPage = Number(req.params.page) || 1;
+    const offset = (curPage - 1) * LIMIT;
 
-  let query = `SELECT * FROM users WHERE 1=1`;
-  let countQuery = `SELECT count(*) as count FROM users WHERE 1=1`;
+    let query = `SELECT * FROM users WHERE 1=1`;
+    let countQuery = `SELECT count(*) as count FROM users WHERE 1=1`;
 
-  if (name) {
-    query += ` AND name = ?`;
-    countQuery += ` AND name = ?`;
-    queryParams.push(name);
-    countparams.push(name);
-  }
-  if (gender) {
-    query += ` AND gender = ?`;
-    countQuery += ` AND name = ?`;
-    queryParams.push(gender);
-    countparams.push(gender);
-  }
+    if (name) {
+        query += ` AND name = ?`;
+        countQuery += ` AND name = ?`;
+        queryParams.push(name);
+        countparams.push(name);
+    }
+    if (gender) {
+        query += ` AND gender = ?`;
+        countQuery += ` AND name = ?`;
+        queryParams.push(gender);
+        countparams.push(gender);
+    }
 
-  query += ` LIMIT ? OFFSET ?`;
-  queryParams.push(LIMIT);
-  queryParams.push(offset);
+    query += ` LIMIT ? OFFSET ?`;
+    queryParams.push(LIMIT);
+    queryParams.push(offset);
 
-  //아무 페이지값도 전달하지 않았을 땐 curPage가 1
-  const stmt = db.prepare(query);
-  const data = stmt.all(queryParams);
-  //어떻게하면 배열로 1,2,3,4,5,6 ... last - 3, last - 2, last - 1, ...이쁘게 전달해줄 수 있을까?
-  //전체페이지
-  const totalArr = paging(curPage, countQuery, countparams);
-  res.json({
-    data: data,
-    page: curPage,
-    totalArr: totalArr,
-    name: name,
-    gender: gender,
-  });
+    const stmt = db.prepare(query);
+    const data = stmt.all(queryParams);
+
+    const totalArr = paging(curPage, countQuery, countparams);
+    res.json({
+        data: data,
+        page: curPage,
+        totalArr: totalArr,
+        name: name,
+        gender: gender,
+    });
 });
 router.get("/user/:id", (req, res) => {
-  const userId = req.params.id;
-  const query = `SELECT * FROM users WHERE id = ?`;
-  const stmt = db.prepare(query);
-  const user = stmt.get(userId);
-  // 주문 정보 구하기 -- orderId와 날짜, 매장 정보를 출력
-  const orderQuery = `
+    const userId = req.params.id;
+    const query = `SELECT * FROM users WHERE id = ?`;
+    const stmt = db.prepare(query);
+    const user = stmt.get(userId);
+    // 주문 정보 구하기 -- orderId와 날짜, 매장 정보를 출력
+    const orderQuery = `
       SELECT o.id as orderId, o.OrdersAt, s.id as storeId, s.Name
       FROM orders o
       JOIN stores s ON s.id = o.StoreId
       JOIN users u ON u.id = o.UserId
       WHERE u.id = ?;`;
-  const orderStmt = db.prepare(orderQuery);
-  const orders = orderStmt.all(userId);
+    const orderStmt = db.prepare(orderQuery);
+    const orders = orderStmt.all(userId);
 
-  // 매장 방문수 구하기
-  const visitQuery = `SELECT (s.type ||' '|| s.name) as name, COUNT(*) AS visitCount
+    // 매장 방문수 구하기
+    const visitQuery = `SELECT (s.type ||' '|| s.name) as name, COUNT(*) AS visitCount
     FROM users u
     JOIN orders o ON o.UserId = u.id
     JOIN stores s ON s.id = o.StoreId
@@ -114,11 +112,11 @@ router.get("/user/:id", (req, res) => {
     GROUP BY s.id
     ORDER BY COUNT(*) DESC
     LIMIT 5;`;
-  const visitStmt = db.prepare(visitQuery);
-  const visitRanking = visitStmt.all(userId);
+    const visitStmt = db.prepare(visitQuery);
+    const visitRanking = visitStmt.all(userId);
 
-  // 물품 주문수 구하기
-  const purchaseQuery = `SELECT i.name, COUNT(*) as purchaseCount
+    // 물품 주문수 구하기
+    const purchaseQuery = `SELECT i.name, COUNT(*) as purchaseCount
       FROM users u
       JOIN orders o ON o.UserId = u.id
       JOIN orderitem oi on oi.OrderId = o.id
@@ -127,14 +125,14 @@ router.get("/user/:id", (req, res) => {
       GROUP BY i.name
       ORDER BY COUNT(*) DESC
       LIMIT 5;`;
-  const purchaseStmt = db.prepare(purchaseQuery);
-  const purchaseRanking = purchaseStmt.all(userId);
+    const purchaseStmt = db.prepare(purchaseQuery);
+    const purchaseRanking = purchaseStmt.all(userId);
 
-  res.json({
-    user: user,
-    orders: orders,
-    visitRanking: visitRanking,
-    purchaseRanking: purchaseRanking,
-  });
+    res.json({
+        user: user,
+        orders: orders,
+        visitRanking: visitRanking,
+        purchaseRanking: purchaseRanking,
+    });
 });
 module.exports = router;
--- 6.CRM/crm_project_v2/routes/login.js
+++ 6.CRM/crm_project_v2/routes/login.js
@@ -1,31 +1,32 @@
-const express = require("express");
-const path = require("path");
-const router = express.Router();
-const sqlite3 = require("better-sqlite3");
+// const express = require("express");
+// const path = require("path");
+// const router = express.Router();
+// const sqlite3 = require("better-sqlite3");
 
-const app = express();
-const db = new sqlite3("db-sample.db");
+// const app = express();
+// const db = new sqlite3("db-sample.db");
 
-app.use(express.json());
+// app.use(express.json());
 
-router.post("/login", (req, res) => {
-  const isLogin = req.session.isLogin || false;
-  const { email, password } = req.body;
-  if (!isLogin) {
-    const loginQuery = `SELECT * FROM admin WHERE email = ? AND password = ?;`;
-    const stmt = db.prepare(loginQuery);
-    const data = stmt.get([email, password]);
-    console.log("로그인 정보", data);
-    if (!data) {
-      req.session.isLogin = false;
-      res
-        .status(401)
-        .json({ isLogin: false, message: "이메일이나 비밀번호가 옳지 않음" });
-    } else {
-      req.session.isLogin = true;
-      res.json({ isLogin: true, message: "로그인 성공" });
-    }
-  }
-});
+// router.post("/login", (req, res) => {
+//     const isLogin = req.session.isLogin || false;
+//     const { email, password } = req.body;
+//     if (!isLogin) {
+//         const loginQuery = `SELECT * FROM admin WHERE email = ? AND password = ?;`;
+//         const stmt = db.prepare(loginQuery);
+//         const data = stmt.get([email, password]);
+//         console.log("로그인 정보", data);
+//         if (!data) {
+//             req.session.isLogin = false;
+//             res.status(401).json({
+//                 isLogin: false,
+//                 message: "이메일이나 비밀번호가 옳지 않음",
+//             });
+//         } else {
+//             req.session.isLogin = true;
+//             res.json({ isLogin: true, message: "로그인 성공" });
+//         }
+//     }
+// });
 
-module.exports = router;
+// module.exports = router;
